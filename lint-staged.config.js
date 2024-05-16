88ba804062889471a4b2bc20a8f6979462b97c69
steelburn <steelburn@gmail.com>
2024-05-16 10:00:59 +0800

    comment

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,13 @@
+// This module exports a single object with a '*.js' property.
+// The '*.js' property is a shorthand for matching all files in the project
+// with the '.js' file extension.
+
 module.exports = {
+  // This property's value is an array of two commands to be executed in order.
   '*.js': ['prettier --write --single-quote', 'git add']
+  // The first command in the array is 'prettier --write --single-quote',
+  // which is a code formatting tool that will automatically format all
+  // matched '.js' files with single quotes and save the changes.
+  // The second command is 'git add', which will stage all changes made
+  // by the previous command for commit.
 };
